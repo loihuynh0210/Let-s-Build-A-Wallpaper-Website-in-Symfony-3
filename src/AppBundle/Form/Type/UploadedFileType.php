c8535c7efd51a35cc735ef7a6fabee2ecfd22907
loihuynh <loihd@vn.s-cubism.jp>
2017-09-21 10:11:53 +0700

    error upload file

--- src/AppBundle/Form/Type/UploadedFileType.php
+++ src/AppBundle/Form/Type/UploadedFileType.php
@@ -6,6 +6,8 @@ use AppBundle\File\SymfonyUploadedFile;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UploadedFileType extends AbstractType
@@ -14,9 +16,17 @@ class UploadedFileType extends AbstractType
     {
         $builder
             ->add('file', FileType::class, [
-                'multiple' => false
+                'multiple' => false,
+                'label'    => false,
             ])
         ;
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+
+        $view->vars['full_name'] = $view->vars['full_name'] . '[file]';
     }
 
     public function configureOptions(OptionsResolver $resolver)
